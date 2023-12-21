eb8051d431b84cc4e14cca221812aa6b5f5312b3
YanQi <yanqirj@gmail.com>
2023-12-22 00:15:11 +0800

    feat: layouts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
 
-import LangSelect from './components/LangSelect.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import ProductCard from './components/ProductCard.vue'
-import NavbarTop from './components/NavbarTop.vue'
+import AppHeader from './components/AppHeader.vue'
+// import LangSelect from './components/LangSelect.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import ProductCard from './components/ProductCard.vue'
+// import NavbarTop from './components/NavbarTop.vue'
 </script>
 
 <template>
-  <h2>{{ $t("msg.test") }}</h2>
-  <LangSelect></LangSelect>
-  <NavbarTop></NavbarTop>
-  <HelloWorld  />
-  <ProductCard/>
+  <!-- <h2>{{ $t("msg.test") }}</h2> -->
+  <AppHeader></AppHeader>
+  <!-- <LangSelect></LangSelect> -->
+  <!-- <NavbarTop></NavbarTop> -->
+  <!-- <HelloWorld  /> -->
+  <!-- <ProductCard/> -->
 </template>
 <!-- https://docs.storefrontui.io/v2/vue/blocks/NavbarTop.html -->
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
