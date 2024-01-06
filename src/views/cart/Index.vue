ff1b1241a20a31e5a2275b3796137b0362428668
YanQi <yanqi2020@qq.com>
2024-01-06 18:40:25 +0800

    feat: go home

--- src/views/cart/Index.vue
+++ src/views/cart/Index.vue
@@ -1,8 +1,8 @@
 <template>
-  <table class="table-fixed">
+  <table class="mx-auto table-fixed mb-7 max-w-1620">
     <thead>
       <tr>
-        <th>PRODUCT</th>
+        <th class="text-left">PRODUCT</th>
         <th>PRICE</th>
         <th>QUANTITY</th>
         <th>SUBTOTAL</th>
@@ -11,8 +11,9 @@
     <tbody>
       <tr>
         <td>
-          <div>
-            <p>Wall Suction Soft Durable Bath Massage Brush – BlueFrom wishlist:Default wishlist</p>
+          <div class="flex items-center">
+            <img src="../../assets/images/shop/product-img1.png" class="w-[208px] h-[208px]" alt="" srcset="" />
+            <p class="w-[320px] ml-[30px]">Wall Suction Soft Durable Bath Massage Brush – BlueFrom wishlist:Default wishlist</p>
           </div>
         </td>
         <td>HK$272.95</td>
@@ -38,6 +39,25 @@
       </tr>
     </tbody>
   </table>
+  <div class="flex">
+    <div class="sm:mx-auto sm:w-full sm:max-w-[750px] flex mt-10 text-sm text-gray-500">
+      <img
+        class="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75"
+        src="../../assets/images/login/icon-left-arrow.svg"
+        alt="Your Company"
+      />
+      <span href="#" class="pl-5 font-semibold leading-6 text-gray-400 group-hover:text-gray-900">continue shopping</span>
+    </div>
+
+    <div class="sm:mx-auto sm:w-full sm:max-w-[750px] flex mt-10 text-sm text-gray-500">
+      <img
+        class="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75"
+        src="../../assets/images/login/icon-left-arrow.svg"
+        alt="Your Company"
+      />
+      <span href="#" class="pl-5 font-semibold leading-6 text-gray-400 group-hover:text-gray-900">refresh package</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
